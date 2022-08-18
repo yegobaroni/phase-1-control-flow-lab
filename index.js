a22b80b771c2a498a84ab28b4dd1c502fbb91939
yego <yegokipkemoi254@gmail.com>
2022-08-18 15:41:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+
+  if (ride <= 400) {
+return "This one is on me!"; }
+ else if (ride > 2000 && ride < 2500){
+     return"I will gladly take your thirty bucks.";
+  }
+  else(ride > 2500); {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else{
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+  } else if (tip === "not as generous") {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
 }
